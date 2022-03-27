3354f949a8e3fca4cff5deabdf4ffebb0448b981
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-03-27 16:49:17 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import "./../styles/App.css";
 function App() {
   return (
     <div id="main">
-      // Do not alter the main div
+      <button onClick="handleClick" id="click"></button>
     </div>
   );
 }
