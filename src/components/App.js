4b6b21608954abc91a460e0c89e86813ccd1a861
Ajit Singh <86841688+ajit-original@users.noreply.github.com>
2022-03-27 16:52:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,15 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
+const handleClick = () => {
+  let para = document.createElement('p');
+  para.textContent = "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
+  document.querySelector("#main").appendChild(para);
+}
+
+
+
+
 function App() {
   return (
     <div id="main">
